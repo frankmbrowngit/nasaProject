fd02d57d83f0c065991ad1d1c78c781ee2570260
Frank Brown <frankbrowngit@gmail.com>
2022-07-10 01:26:24 -0400

    Second commit - got the server to serve the frontend

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const planetsRouter = require('./routes/planets/planets.router.js');
+const path = require('path');
 // JSON parsing middleware
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname,'..','public')));
 app.use(planetsRouter);
 
 module.exports = app;
